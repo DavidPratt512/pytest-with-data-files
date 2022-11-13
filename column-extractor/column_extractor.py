19e51a45bf09842d0a583db443ed1625e0f084f5
David Pratt <davidpratt512@gmail.com>
2022-11-12 21:12:12 -0700

    Implement a better column extractor

--- column-extractor/column_extractor.py
+++ column-extractor/column_extractor.py
@@ -1,5 +1,9 @@
+import re
 from typing import Set
 
 
+COLUMN_REGEX = re.compile(r'\[(\w+)\]')
+
+
 def extract_column(sql: str) -> Set[str]:
-    return set()  # TODO
+    return set(COLUMN_REGEX.findall(sql))
---
+++ column-extractor/test/column-extractor-test-cases/all-caps/given.sql
@@ -0,0 +1,6 @@
+CREATE TABLE EMP_TBL
+(
+    [ID]     INT PRIMARY KEY,
+    [NAME]   VARCHAR (128),
+    [SALARY] NUMERIC (12, 4)
+);
